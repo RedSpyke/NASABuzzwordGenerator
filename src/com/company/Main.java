ef3a2804f7a038abe641c2d7d9a81e39b7f602ee
Alexandru Palcau <alexandrupalcau@outlook.com>
2020-04-11 11:30:38 +0300

    NASA words

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        String[] column1 = {"integrated", "total", "systematized", "parallel", "functional", "responsive", "optimal",
+                "synchronized", "compatible", "balance"};
+        String[] column2 = {"Management", "organizational", "monitored", "reciprocal", "digital", "logistical", "transitional", "incremental",
+                "third-generation", "policy"};
+        String[] column3 = {"options", "Flexibility", "capability", "mobility", "programming", "concept", "time-phase",
+                "projection", "hardware", "contingency"};
+
+
+    }
+}
